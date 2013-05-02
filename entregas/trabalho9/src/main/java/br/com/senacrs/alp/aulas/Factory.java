d16854e538662f31d141059bde2248e376037344
Rafael <Rafael@rafael-pc>
2013-05-02 20:08:02 -0300

    Trabalho9

--- entregas/trabalho9/src/main/java/br/com/senacrs/alp/aulas/Factory.java
+++ entregas/trabalho9/src/main/java/br/com/senacrs/alp/aulas/Factory.java
@@ -1,21 +1,21 @@
 package br.com.senacrs.alp.aulas;
 
 public class Factory {
-	
+
 	private static final Factory instancia = new Factory();
-	
+
 	private Factory() {
 	}
-	
+
 	public EmissorMensagens criarEmissor(String arquivoEntrada) {
-	
+
 		EmissorMensagens resultado = null;
-		
-		//implementar
-		
+
+		resultado = new MeuEmissor(arquivoEntrada);
+
 		return resultado;
 	}
-	
+
 	public static Factory getInstancia() {
 		return instancia;
 	}
