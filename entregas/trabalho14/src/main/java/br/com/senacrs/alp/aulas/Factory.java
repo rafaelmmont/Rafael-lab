5bff2fca681a47d534aa1b8517f4e8eda831e763
Rafael <Rafael@rafael-pc>
2013-06-17 22:57:06 -0300

    Trabalho 14

---
+++ entregas/trabalho14/src/main/java/br/com/senacrs/alp/aulas/Factory.java
@@ -0,0 +1,20 @@
+package br.com.senacrs.alp.aulas;
+
+public class Factory {
+	private static final Factory instancia = new Factory();
+
+	private Factory() {
+	}
+
+	public ObterPathRequisicaoGet criarValidacao() {
+		
+		//implementar
+		ObterPathRequisicaoGet resultado = null;
+		resultado = new MinhaCOP();
+		return resultado;
+	}
+
+	public static Factory getInstancia() {
+		return instancia;
+	}
+}
---
+++ entregas/trabalho14/src/main/java/br/com/senacrs/alp/aulas/ObterPathRequisicaoGet.java
@@ -0,0 +1,9 @@
+package br.com.senacrs.alp.aulas;
+
+import java.io.Reader;
+
+public interface ObterPathRequisicaoGet {
+	public static final String NOVA_LINHA = "\r\n";
+
+	String caminhoAbsoluto(Reader requisicao);
+}
