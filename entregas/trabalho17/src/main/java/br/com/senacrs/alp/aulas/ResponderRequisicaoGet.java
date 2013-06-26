ed4d9ac913414dbc1f513dc07d20bac38c46ad90
Rafael <Rafael@rafael-pc>
2013-06-26 20:33:44 -0300

    Trabalho 17

--- entregas/trabalho17/src/main/java/br/com/senacrs/alp/aulas/ResponderRequisicaoGet.java
+++ entregas/trabalho17/src/main/java/br/com/senacrs/alp/aulas/ResponderRequisicaoGet.java
@@ -5,7 +5,7 @@ import java.io.Writer;
 import java.util.Date;
 
 public interface ResponderRequisicaoGet {
-		
+
 	public static final String NOVA_LINHA = "\r\n";
 	public static final Date DATE = new Date();
 	public static final String SERVER = "MeuServidorJava";
